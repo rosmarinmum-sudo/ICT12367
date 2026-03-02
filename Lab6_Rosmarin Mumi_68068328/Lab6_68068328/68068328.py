c71af9ebb1f5ca007d726d0b81b9f495453dc0ca
rosmarinmum-sudo <rosmarin.mum@spumail.net>
2026-03-02 22:46:49 +0700

    Add files via upload

---
+++ Lab6_Rosmarin Mumi_68068328/Lab6_68068328/68068328.py
@@ -0,0 +1,10 @@
+print("Hello python")
+
+age = 25
+print("อายุของฉันคือ", age)
+
+name = "รสมาริน มุมิ"
+age = 22 
+print(f"สวัสดี {name}, อายุ {age} ปี")
+
+print("Python", "Java", "C++", sep=" | ")
